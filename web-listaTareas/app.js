63626e133a6be59b91960ce606a0e4e0342abf7e
Teo <teo.lairla@iessierradeguara.com>
2020-10-23 17:04:17 +0200

    añadido boton borrar y papelera

--- web-listaTareas/app.js
+++ web-listaTareas/app.js
@@ -1,8 +1,40 @@
 let lista = document.getElementById("listaTareas");
 let tarea = document.getElementById("inputTarea");
 function agregarTarea() {
-    let liTarea = document.createElement("li");
-    liTarea.innerHTML = tarea.value;
-    lista.appendChild(liTarea);
-    tarea.value = "";
+    if (tarea.value != "") {
+        let liTarea = document.createElement("li");
+        let liTexto = document.createElement("span");
+        let papelera = document.createElement("img");
+        papelera.src = "./img/trash.svg";
+        papelera.className="papelera";
+        papelera.addEventListener("click",borrarEstaTarea);
+        liTexto.innerHTML = tarea.value;
+        liTarea.appendChild(liTexto);
+        liTarea.appendChild(papelera);
+        lista.appendChild(liTarea);
+        tarea.value = "";
+    }
+    tarea.focus();
+}
+
+function borrarEstaTarea(evento) {
+    evento.target.parentElement.remove();
+}
+
+
+function gestionarTecla(evento) {
+    // console.log(evento);
+    if (evento.keyCode == 13) {
+        agregarTarea();
+    }
+}
+
+function borrarTarea() {
+    let valorDevuelto = prompt("Escribe el número de tarea que quieres borrar:");
+    if(valorDevuelto > 0 && valorDevuelto <= lista.childElementCount ){
+        lista.children[valorDevuelto-1].remove();
+        // lista.removeChild(lista.children[valorDevuelto-1]);  
+    }else{
+      alert("Esa tarea no existe!");  
+    }
 }
