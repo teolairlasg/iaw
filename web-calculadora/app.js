cbba0e311d4f6854e54d0ef06f7e29a5b3f95c06
Teo <teo.lairla@iessierradeguara.com>
2020-10-28 17:30:03 +0100

    Cambiado el tofixed a dentro del if

--- web-calculadora/app.js
+++ web-calculadora/app.js
@@ -7,22 +7,28 @@ function imprimirSuma() {
     TODO rellenar el párrafo con id resultado sólo si el resultado no es null 
     */
 
-    
-    document.getElementById("res").innerHTML= resultado;
+    if (resultado != null) {
+        document.getElementById("res").innerHTML= resultado;    
+    }else{
+        document.getElementById("res").innerHTML= "Error en los operandos";    
+    }
     
 }
 
 
 function suma(o1, o2) {
-    let resultado;
+    let resultado=0;
     /* TODO: 
      - Comprobar que los parámetros son números
-     - Si los dos son números sumarlos en la variable resultado
+     - Si los dos son números sumarlos (convertirlos a números con parse) en la variable resultado isNaN(a) devuelve cierto si el valor de a, no es un número.
      - Si alguno de los dos no es número resultado = null
      
      */
-
-
-
+    
+     if(isNaN(o1) == true ||  isNaN(o2) == true ){
+        resultado = null;
+     }else{
+        resultado = (parseFloat(o1) +  parseFloat(o2)).toFixed(2);
+     }
     return resultado;
 }
