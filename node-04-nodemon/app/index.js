a363e13e0491d5be2e40769333b77dfa127df934
Teo <teo.lairla@iessierradeguara.com>
2021-01-20 18:03:33 +0100

    nodemon index.html

--- node-04-nodemon/app/index.js
+++ node-04-nodemon/app/index.js
@@ -1,1 +1,25 @@
-console.log("hola mundoo");
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('puerto', 3000);
+
+app.get('/', 
+    function (req,res) {
+        let pathToIndex = 
+            path.join(__dirname, "public", "index.html");
+        res.sendFile(pathToIndex);      
+    }
+);
+
+
+
+app.listen(app.get('puerto'), inicioServidor());
+
+function inicioServidor() {
+    console.log(
+        `Servidor iniciado en 
+http://localhost:${app.get("puerto")}`);
+}
+
+
