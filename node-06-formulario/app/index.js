fe58fe5ab3cd60bde57181da75dab96d715758ab
Teo <teo.lairla@iessierradeguara.com>
2021-01-27 18:01:49 +0100

    formulario con body-parser

--- node-06-formulario/app/index.js
+++ node-06-formulario/app/index.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const path = require('path');
-
+//para usar body-parser
+const bodyParser = require('body-parser');
 const app = express();
 app.set('puerto',3000);
-// todo usar body Parser y sanitizar datos.
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,"form.html"));
 });
 
 app.post('/form', function (req,res) {
-  //todo capturar datos
+  console.log("peticion recibida: "+req.method);
+/*
+// Método sin libreria body-parser
+  let body = '';
+  req.on('data', function (dato) {
+    body+= dato.toString();
+  });
+  req.on('end', function () {
+    console.log(body);
+    res.sendFile(path.join(__dirname,"form.html"));
+  }); 
+*/
+
+//con body-parser
+  let recibido = req.body;
+  console.log(`El usuario ha introducido ${recibido.vName} en el campo Nombre y ${recibido.vSurname} en el campo Apellido`);
+  res.sendFile(path.join(__dirname,"form.html"));
+
 })
 
 
