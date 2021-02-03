973d48c22ca5c5f3cbbdb915a6ad6588129724f2
Teo <teo.lairla@iessierradeguara.com>
2021-02-03 17:28:43 +0100

    ejs2

--- node-08-ejs2/app/index.js
+++ node-08-ejs2/app/index.js
@@ -9,11 +9,15 @@ app.set('view engine', 'ejs');
 app.set('views',dirVistas);
 
 app.get('/', function (req, res) {
-
  res.render('paginas/inicio',
-  { texto: "Hola Mundo" });
+  { texto: "Hola Mundo", ip: req.ip, ruta: req.path, protocolo: req.protocol, urlBase: req.baseUrl,
+  seguro: req.secure });
 });
 
+app.get('/style.css', function (req,res) {
+  let rutaCSS = path.join(__dirname,'css','style.css');
+  res.sendFile(rutaCSS);
+})
 
 app.listen(app.get('puerto'),
  function () {
