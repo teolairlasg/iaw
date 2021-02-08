20ca1b077093c2522bd04a5d9f676e3ba5094ffa
Teo <teo.lairla@iessierradeguara.com>
2021-02-08 20:13:48 +0100

    Web 10 finalizada

--- node-10-ejercicio/cliente/app.js
+++ node-10-ejercicio/cliente/app.js
@@ -1,13 +1,19 @@
 alert('Aun no hemos empezado');
 
-fetch('http://localhost:3000')
+fetch('http://localhost:3000/lista')
     .then(respuesta => respuesta.json()) //convertir la respuesta a json
-    .then(objeto => rellenarBody(objeto.lista))
+    .then(objeto => rellenarLista(objeto.lista))
+    .catch(error => console.error('Fallo:', error));
+
+
+fetch('http://localhost:3000/texto')
+    .then(respuesta => respuesta.json()) //convertir la respuesta a json
+    .then(objeto => rellenarTexto(objeto.texto))
     .catch(error => console.error('Fallo:', error));
 
 alert('Ya hemos empezado');
 
-function rellenarBody(lista) {
+function rellenarLista(lista) {
     let elementoUl = document.createElement('ul');
     document.body.appendChild(elementoUl);
     for (const texto of lista) {
@@ -15,4 +21,10 @@ function rellenarBody(lista) {
         elementoLi.innerHTML = texto;
         elementoUl.appendChild(elementoLi);
     }
+}
+
+function rellenarTexto(txt) {
+    let elementoP = document.createElement('p');
+    elementoP.innerHTML = txt;
+    document.body.appendChild(elementoP);
 }
--- node-10-ejercicio/servidor/index.js
+++ node-10-ejercicio/servidor/index.js
@@ -2,15 +2,27 @@ const express = require('express');
 const app = express();
 const puerto = 3000;
 
-app.get('/', function (req,res) {
+app.get('/lista', function (req,res) {
     let respuesta = {
         "lista": ["Hola", "adios","alumno","mundo"]
-    }
+    };
     // Permitir conexiones desde otros origenes.
     //Necesario para xhttp response
     res.header("Access-Control-Allow-Origin", "*");
     res.json(respuesta);
-})
+});
+
+app.get('/texto', function (req,res) {
+    let respuesta = {
+        "texto": "Hola Mundo"
+    };
+    // Permitir conexiones desde otros origenes.
+    //Necesario para xhttp response
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json(respuesta);
+});
+
+
 
 app.listen(puerto, iniciaServidor());
 
