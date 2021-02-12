30fad872bbe7da72afd1c12662793143b80b4422
Teo <teo.lairla@iessierradeguara.com>
2021-02-12 16:32:52 +0100

    parciales con parametros

--- node-12-webstatic/index.js
+++ node-12-webstatic/index.js
@@ -6,13 +6,21 @@ const puerto = 3000;
 let rutaPublic = path.join(__dirname, 'public');
 app.use(express.static(rutaPublic));
 app.set('view engine', 'ejs');
+
+let datos = {
+    'itemsAside': ['main', 'pagina2', 'pagina3', 'pagina4', 'pagina5'],
+    'pagina': 'main'
+};
+
 app.get('/', function (req, res) {
-    let listaAside = {
-        'items': ['uno', 'dos', 'tres']
-    };
-    res.render('index',listaAside);
+    datos.pagina='main';
+    res.render('index',datos);
 })
 
+app.get('/:pag',function (req,res) {
+   datos.pagina=req.params.pag;
+   res.render('index', datos);
+});
 
 app.listen(puerto, iniciaServidor());
 
